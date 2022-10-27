29b91d693384621a1025e9f9559ef3be148ffd0b
Eduardo Unrrein <gum23coder@gmail.com>
2022-10-27 00:37:14 -0300

    Integramos y dimos funcionalidad al menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguer = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail-addEventListener('click', toggleDesktopMenu);
+menuBurguer-addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
+}
