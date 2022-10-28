c24a827671f35a999ed7b9bab70016e90aa21bbe
Eduardo Unrrein <gum23coder@gmail.com>
2022-10-28 17:00:56 -0300

    Agrtegamos funcionalidad a carrito y menúes en desktop y mobile

--- main.js
+++ main.js
@@ -2,15 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurguer = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail-addEventListener('click', toggleDesktopMenu);
-menuBurguer-addEventListener('click', toggleMobileMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     menuMobile.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = menuMobile.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        menuMobile.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive'); 
+    }
+
+    aside.classList.toggle('inactive');
 }
